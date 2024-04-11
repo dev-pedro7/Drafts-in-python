6096d172b12e9aacc7d5b3de0707dff8aa56f51f
dev-pedro7 <pedr2046@gmail.com>
2024-04-11 06:21:45 -0300

    exercicios carro

--- composicao.py
+++ composicao.py
@@ -1,4 +1,4 @@
-#Exercício com classes
+# Exercício com classes
 # 1 - Crie uma classe Carro (Nome)
 # 2 - Crie uma classe Motor (Nome)
 # 3 - Crie uma classe Fabricante (Nome)
@@ -16,26 +16,51 @@ class Carro:
     @property
     def motor(self):
         return self._motor
-    
+
     @motor.setter
-    def motor(self,valor):
+    def motor(self, valor):
         self._motor = valor
 
     @property
     def fabricante(self):
-        return self._fabricante 
-       
+        return self._fabricante
+
     @fabricante.setter
-    def fabricante(self,valor):
+    def fabricante(self, valor):
         self._fabricante = valor
+
 
 class Motor:
     def __init__(self, nome):
-        self._motor = nome
+        self.nome = nome
+
 
 class Fabricante:
     def __init__(self, nome):
-        self._fabricante = nome
+        self.nome = nome
 
-opala = Carro('Fusca')
-corolla = Carro('Corolla')
+
+fusca = Carro('Fusca')
+volkswagen = Fabricante('Volkswagen')
+motor_1_0 = Motor('1.0')
+fusca.fabricante = volkswagen
+fusca.motor = motor_1_0
+print(fusca.nome, fusca.fabricante.nome, fusca.motor.nome)
+
+gol = Carro('Gol')
+gol.fabricante = volkswagen
+gol.motor = motor_1_0
+print(gol.nome, gol.fabricante.nome, gol.motor.nome)
+
+fiat_uno = Carro('Uno')
+fiat = Fabricante('Fiat')
+fiat_uno.fabricante = fiat
+fiat_uno.motor = motor_1_0
+print(fiat_uno.nome, fiat_uno.fabricante.nome, fiat_uno.motor.nome)
+
+focus = Carro('Focus Titanium')
+ford = Fabricante('Ford')
+motor_2_0 = Motor('2.0')
+focus.fabricante = ford
+focus.motor = motor_2_0
+print(focus.nome, focus.fabricante.nome, focus.motor.nome)
