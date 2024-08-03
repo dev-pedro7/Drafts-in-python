255075a3dfc29a1cdefd81178c616d240370d3ab
Pedro <157183934+dev-pedro7@users.noreply.github.com>
2024-08-03 19:31:32 -0300

    Update lista.py

--- basic/lista.py
+++ basic/lista.py
@@ -3,11 +3,12 @@ Listas em Python
 Tipo list - Mutável
 Suporta vários valores de qualquer tipo
 Conhecimentos reutilizáveis - índices e fatiamento
+
 Métodos úteis:
     append, insert, pop, del, clear, extend, +
 Create Read Update   Delete
 Criar, ler, alterar, apagar = lista[i] (CRUD)
-
+"""
 #        0   1   2   3   4   5
 lista = [10, 20, 30, 40]
 # lista[2] = 300
